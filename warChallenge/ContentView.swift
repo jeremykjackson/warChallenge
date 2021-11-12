1e1e29e6b56b9d25480cf0f2fba5c4407ffa9951
Jeremy Jackson <jeremykjackson@me.com>
2021-11-11 20:15:51 -0600

    Initial Commit

---
+++ warChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warChallenge
+//
+//  Created by Jeremy Jackson on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warChallenge/warChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeApp.swift
+//  warChallenge
+//
+//  Created by Jeremy Jackson on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
