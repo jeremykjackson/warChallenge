2ede0445182b710eb951b4977f8d0ffc639b63d0
jeremykjackson <jeremykjackson@me.com>
2021-11-29 21:18:58 -0600

    Dealing random Cards

--- warChallenge/ContentView.swift
+++ warChallenge/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card7"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Image("background").ignoresSafeArea()
+            VStack(){
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack(){
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    // Generate Random number for Card
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                     
+                    //Update Cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    //Update Score
+                    cpuScore += 1
+                    
+                }, label: {
+                    Image("dealbutton")
+                } ) 
+                
+                
+                Spacer()
+                HStack(){
+                    Spacer()
+                    VStack(){
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack(){
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+              Spacer()
+                }
+               
+            }
+        
+        
     }
 }
 
